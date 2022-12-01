706bde80fb116a030054edb1e40e0c5ab5f022a9
Mobin <111445335+Mwbie@users.noreply.github.com>
2022-12-01 14:33:13 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+﻿let firstValue = document.querySelector('.C')
+let secondValue = document.querySelector('.F')
+let converter = document.querySelector('#converter')
+let result = document.querySelector('.result')
+let convertButton = document.querySelector('.convertButton')
+let resetButton = document.querySelector('.resetButton')
+let changeButton = document.querySelector('.changeButton')
+
+function convert() {
+    if(firstValue.innerHTML === '°C' ){
+
+     if(isNaN(converter.value)  ){
+        result.innerHTML = "please enter a valid number"
+        result.style.color = "red"
+    }else{
+        let celicios = converter.value
+        let converted = celicios * 9.5 + 32
+        result.innerHTML = converted.toFixed(2) + ' °F'
+        converter.value = ""
+        result.style.color = "yellow"
+}       
+    }else{
+
+        if(isNaN(converter.value)  ){
+            result.innerHTML = "please enter a valid number"
+            result.style.color = "red"
+        }else{
+            let farenheight = converter.value
+            let converted = (farenheight-32) * 5 / 9
+            result.innerHTML = converted.toFixed(2) + " °C"
+            result.style.color = "yellow"
+        }
+  
+       
+    }
+}
+function reset() {
+    converter.value = ''
+    result.innerHTML= ''
+ 
+}
+function swap() {
+    if(firstValue.innerHTML === "°C"){
+     firstValue.innerHTML = "°F"
+     secondValue.innerHTML = "°C"
+     document.title = "°F to °C"
+     converter.innerHTML = ""
+     converter.placeholder = '°F'
+     result.innerHTML = ""
+
+    }else{
+        firstValue.innerHTML = "°C"
+        secondValue.innerHTML = "°F"
+        document.title = "°C to °F"
+        converter.innerHTML = ""
+        converter.setAttribute("placeholder",'°C')
+        result.innerHTML = ""
+    }
+}
+
+convertButton.addEventListener('click',convert)
+resetButton.addEventListener('click',reset)
+changeButton.addEventListener('click',swap)
